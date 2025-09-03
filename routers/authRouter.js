c50321bb25f9c71c0e67679065eea740581415d5
adeniyi feranmi <adeniyiferanmi2024@gmail.com>
2025-09-03 21:06:27 +0100

    all

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -2,7 +2,7 @@ import express from "express"
 import { signInHandler, signup } from "../controllers/authController.js"
 const authRouter = express.Router()
 
-authRouter.post("/",signup)
+authRouter.post("/signup",signup)
 authRouter.post("/signin",signInHandler)
 
 export default authRouter
